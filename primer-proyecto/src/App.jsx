ed8788d5cd7b228229546646ad9c603cd6b388df
Jeanfranco Sanchez <sanchez.franco22@gmail.com>
2023-04-13 11:55:38 -0300

    Se sube la carpeta primer- proyecto con los archivos de React

---
+++ primer-proyecto/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Grupo 11</h1>
+      <h2>Primer Proyecto</h2>
+      </div>
+  )
+}
+
+export default App
